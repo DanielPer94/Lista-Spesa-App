222b44a68df9441eef27ba69f4c750bf07b68cd5
Daniel Persi <106588771+DanielPer94@users.noreply.github.com>
2024-12-16 16:26:15 +0100

    Spring_Initializer
    Creazione progetto Spring

---
+++ Lista_Spesa_App/src/main/java/Team/Lista_Spesa_App/ListaSpesaAppApplication.java
@@ -0,0 +1,13 @@
+package Team.Lista_Spesa_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaSpesaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaSpesaAppApplication.class, args);
+	}
+
+}
